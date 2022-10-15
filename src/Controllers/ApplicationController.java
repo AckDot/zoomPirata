e69346b505a2adb055d07c68a41a85e58f9cf246
GatoEnBucle <dxgiovanni15@hotmail.com>
2022-10-14 23:11:58 -0400

    Version 2.3

--- src/Controllers/ApplicationController.java
+++ src/Controllers/ApplicationController.java
@@ -1,4 +1,3 @@
-
 package Controllers;
 
 import Models.User;
@@ -11,35 +10,35 @@ import java.awt.event.ActionListener;
  * @author Jorge
  */
 public class ApplicationController {
-    
+
     private final ApplicationGUI gui;
     private final User user;
-    
-    public ApplicationController(User user){
+
+    public ApplicationController(User user) {
         this.user = user;
         gui = new ApplicationGUI(user.getUserName());
         setActionProfileButton();
         setActionMeetButton();
         gui.setVisible(true);
     }
-    
-    
-    private void setActionProfileButton(){
+
+    private void setActionProfileButton() {
         gui.getProfileButton().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ControllerUserProfile c = new ControllerUserProfile(user);
+                gui.setContentPanel(c.getPanel());
+            }
+        });
+    }
+
+    private void setActionMeetButton() {
+        gui.getMeetButton().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
             }
         });
     }
-    
-    private void setActionMeetButton(){
-        gui.getMeetButton().addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
 
-            }
-        });     
-    }
-    
 }
